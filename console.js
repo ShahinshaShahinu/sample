ae694a10b7598c5fff92189d9dcd11cdde93bc58
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-06-12 01:24:09 +0530

    NewChangesMaster

--- console.js
+++ console.js
@@ -11,20 +11,6 @@ console.js('shahinsha');
 
 
 
-console.js('hello world');console.js('hello world');console.js('shahinsha');
-console.js('shahinsha');console.js('hello world');console.js('shahinsha');
-console.js('shahinsha');console.js('shahinsha');
-console.js('shahinsha');console.js('shahinsha');console.js('hello world');
-console.js('hello world');console.js('hello world');console.js('hello world');
-console.js('shahinsha');
-
-
-
-console.js('hello world');console.js('hello world');console.js('shahinsha');
-console.js('shahinsha');console.js('hello world');console.js('shahinsha');
-console.js('shahinsha');console.js('shahinsha');
-console.js('shahinsha');console.js('shahinsha');console.js('hello world');
-
 
 
 
