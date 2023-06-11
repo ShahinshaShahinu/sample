238bce721e67d553bbf400c14a700b18eca81fbf
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-06-12 00:43:28 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,29 +1,29 @@
 
 
-console.js('hello world');
+console.js('hello world');console.js('hello world');
+console.js('shahinsha');
+
+console.js('hello world');console.js('hello world');console.js('hello world');
 console.js('shahinsha');
 
 
-console.js('hello world');
-console.js('shahinsha');
 
-console.js('hello world');
-console.js('shahinsha');
-
-console.js('hello world');
-console.js('shahinsha');
+console.js('hello world');console.js('hello world');console.js('shahinsha');
+console.js('shahinsha');console.js('hello world');console.js('shahinsha');
+console.js('shahinsha');console.js('shahinsha');
+console.js('shahinsha');console.js('shahinsha');console.js('hello world');
 
 
-console.js('hello world');
-console.js('shahinsha');
-
-console.js('hello world');
-console.js('shahinsha');console.js('hello world');
-console.js('shahinsha');
 
 
-console.js('hello world');
-console.js('shahinsha');
 
-console.js('hello world');
-console.js('shahinsha');
+
+
+
+
+
+
+
+
+
+
